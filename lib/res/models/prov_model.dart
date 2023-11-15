f3c6bb027865457be8661a1be49c721d82277c7f
ZainMughal329 <zainmughal4379329@gmail.com>
2023-11-16 02:35:37 +0500

    service provider signup done!

--- lib/res/models/prov_model.dart
+++ lib/res/models/prov_model.dart
@@ -1,38 +1,38 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ProviderModel{
-  String ?id;
+class ProviderModel {
+  String? id;
   final String providerName;
   final String phone;
   final String email;
+  final String service;
   ProviderModel({
-    this.id='',
+    this.id = '',
     required this.providerName,
     required this.phone,
     required this.email,
+    required this.service,
   });
-    toJson(){
-      return{
-  'id':id,
-  'providerName':providerName,
-  'phone':phone,
-  'email':email,
-  };
-    }
-    factory ProviderModel.fromJson(
-      DocumentSnapshot<Map<String , dynamic>>snapshot,
-        ) {
-final json= snapshot.data()!;
-  return ProviderModel(
-        id:json["id"],
-       providerName:json["providerName"],
-      phone:json["phone"],
-           email:json["email"],
-);
-}
-
+  toJson() {
+    return {
+      'id': id,
+      'providerName': providerName,
+      'phone': phone,
+      'email': email,
+      'service': service,
+    };
   }
 
-
-
+  factory ProviderModel.fromJson(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+  ) {
+    final json = snapshot.data()!;
+    return ProviderModel(
+      id: json["id"],
+      providerName: json["providerName"],
+      phone: json["phone"],
+      email: json["email"],
+      service: json["service"],
+    );
+  }
+}
